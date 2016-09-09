4f74e74e9829b4915b8ae307314fb44f3fd53f3a
Galaa <galaagl@yahoo.com>
2016-09-09 16:37:06 +0900

    Linear Regression

---
+++ Linear Regression/Seminar-1.R
@@ -0,0 +1,49 @@
+#
+# Linear Regression
+# Seminar 1
+# Author: galaa
+# Created on 2016/09/09 16:31:00
+#
+
+# Data
+
+Y <- c(5.6, 8.9, 13.1, 19.8, 4.7, 20.5, 20.2, 13.6, 11.4, 3.3)
+X <- c(2.7, 3.7, 5.7, 9.1, 2.0, 9.0, 9.4, 6.6, 6.3, 0.6)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# by using function lm() from the package stats
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+fit <- lm(Y ~ X)
+summary(fit)
+residuals(fit)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Detailed computation
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# estimation of slope b
+
+b <- cor(Y, X) * sd(Y) / sd(X)
+
+print(b)
+
+# estimation of intercept a
+
+a <- mean(Y) - b * mean(X)
+
+print(a)
+
+# coefficient of determination R^2
+
+SST <- sum((Y - mean(Y))^2)
+
+SSR <- sum((Y - (a + b * X))^2)
+
+R.square <- 1 - SSR / SST
+
+print(R.square)
