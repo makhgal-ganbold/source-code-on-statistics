cc3b1b72de28f2a231e96c4dcdcca26bd33169e0
MAKHGAL Ganbold <galaagl@yahoo.com>
2022-12-31 09:11:38 +0800

    Package Development

--- R Language/Package Development/edf.R
+++ R Language/Package Development/edf.R
@@ -2,13 +2,13 @@
 #'
 #' @description Computes the value of the left continuous empirical distribution function at a given point.
 #'
-#' @param x numeric, function argument
-#' @param X vector, empirical data
+#' @param x numeric scalar, function argument
+#' @param X numeric vector, empirical data
 #' @return numeric, left continuous empirical distribution function value
 #' @export
 #' @examples
-#' edf(x = 0, X = c(- 1, 0, 1));
+#' edf(x = 0, X = c(- 1, 0, NA, 1));
 
 edf <- function (x, X) {
-  sum(X < x, na.rm = TRUE) / length(which(!is.na(X)));
+  sum(X < x, na.rm = TRUE) / sum(!is.na(X))
 }
